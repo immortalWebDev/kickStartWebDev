92a17de4453e82825774eae5aee1a43b7ce67fd2
immortalWebDev <pgbadgujar007@gmail.com>
2024-02-13 13:27:45 +0530

    getElementsByClassName

---
+++ index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+var fruits = document.getElementsByClassName("fruit")
+fruits[2].style.backgroundColor = "yellow" 
+
+for(var i = 0;i<fruits.length;i++)
+  {
+    fruits[i].style.fontWeight = "bold"
+  }
