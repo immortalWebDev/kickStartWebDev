42ed8798a2cd15a56e8fc0883d0dedcb36059227
immortalWebDev <pgbadgujar007@gmail.com>
2024-02-13 19:29:31 +0530

    Get element by ID

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+var heading = document.getElementById("header");
+heading.style.backgroundColor = "green";
+heading.style.borderBottom = "3px solid orange";
+
+var basket = document.getElementById("basket-heading");
+basket.style.color = "green";
+
+var last = document.getElementById("thanks");
+last.innerHTML = "<p>Please visit us again</p>";
+
